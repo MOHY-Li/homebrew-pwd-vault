8baf29a928beaf2f88cc005681524d4141da5986
moyang <moyang@moyangdeMacBook-Pro.local>
2026-04-16 00:58:08 +0800

    Update Pwd-Vault to v0.2.0 (Touch ID support)

--- Casks/pwd-vault.rb
+++ Casks/pwd-vault.rb
@@ -1,10 +1,10 @@
 cask "pwd-vault" do
-  version "0.1.0"
-  sha256 "ef9118fb56d55ce01fcd88373030d87cbbbf2585f79381c6cd3cfd3fe41aa212"
+  version "0.2.0"
+  sha256 "b1efb71f9854e58e615fa69e01cc04ede75e40dc2b1be8223c05d850f6425624"
 
-  url "https://github.com/MOHY-Li/pwd-vault/releases/download/v0.1.0/Pwd-Vault_0.1.0_aarch64.dmg"
+  url "https://github.com/MOHY-Li/pwd-vault/releases/download/v0.2.0/Pwd-Vault_0.2.0_aarch64.dmg"
   name "Pwd-Vault"
-  desc "安全的本地密码管理器 - AES-256加密, TOTP, 审计日志"
+  desc "安全的本地密码管理器 - AES-256加密, TOTP, Touch ID, 审计日志"
   homepage "https://github.com/MOHY-Li/pwd-vault"
 
   depends_on macos: ">= :ventura"
