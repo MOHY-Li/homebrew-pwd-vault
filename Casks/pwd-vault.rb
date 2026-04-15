4950752a8a69ce5e355216ae6fbc142483b072f4
moyang <moyang@moyangdeMacBook-Pro.local>
2026-04-16 02:00:27 +0800

    Update sha256 for v0.1.1

--- Casks/pwd-vault.rb
+++ Casks/pwd-vault.rb
@@ -1,6 +1,6 @@
 cask "pwd-vault" do
   version "0.1.1"
-  sha256 "c6811f8859eaf92ed4f38087dec6f662a4faf6e934d30f263e60b0801733b040"
+  sha256 "cd213d598cf677664a127ee6f31b2925403b69183e42bfb03183411632b2596e"
 
   url "https://github.com/MOHY-Li/pwd-vault/releases/download/v0.1.1/Pwd-Vault_0.1.1_aarch64.dmg"
   name "Pwd-Vault"
