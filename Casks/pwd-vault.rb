6acf5359271afde221f3c517ba94a9f70d4409db
moyang <moyang@moyangdeMacBook-Pro.local>
2026-04-17 00:07:18 +0800

    Bump to v0.2.0

--- Casks/pwd-vault.rb
+++ Casks/pwd-vault.rb
@@ -1,8 +1,8 @@
 cask "pwd-vault" do
-  version "0.1.1"
-  sha256 "36c7dac7ded1e6716f4f2692cb829a0aca5683fa5e83889790ae6be636493d2c"
+  version "0.2.0"
+  sha256 "4d2a506ee490ecca70cc47fb2dfe85339ae83f11c651d6f7dee0acd1bb6af78d"
 
-  url "https://github.com/MOHY-Li/pwd-vault/releases/download/v0.1.1/Pwd-Vault_0.1.1_aarch64.dmg"
+  url "https://github.com/MOHY-Li/pwd-vault/releases/download/v0.2.0/Pwd-Vault_0.2.0_aarch64.dmg"
   name "Pwd-Vault"
   desc "安全的本地密码管理器 - AES-256加密, TOTP, 审计日志"
   homepage "https://github.com/MOHY-Li/pwd-vault"
